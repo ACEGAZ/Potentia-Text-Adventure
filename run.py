6aca8fa62a7e562a2db30a19fe60d363bcabb5f2
ACEGAZ <garylowles@gmail.com>
2022-05-10 13:45:29 +0000

    add enemy class for combat and password to be used at part 3 of the story

--- run.py
+++ run.py
@@ -1,2 +1,22 @@
 import time
 import random
+
+
+class Enemy:
+    """
+    class for all enemies
+    """
+    def __init__(self, name):
+        self.name = name
+
+
+enemy = Enemy("Sailor")
+
+
+PASSWORD = ["Nadaren",
+            "nadaren",
+            "NADAREN",
+            "Nadaren.",
+            "nadaren.",
+            "NADAREN."
+            ]
